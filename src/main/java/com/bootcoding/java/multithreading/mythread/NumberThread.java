73adf5c5adb24c9913bd53dab0f35976ee3c3ad0
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-09 22:35:06 +0530

    NUmber thread

---
+++ src/main/java/com/bootcoding/java/multithreading/mythread/NumberThread.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.multithreading.mythread;
+
+public class NumberThread extends Thread{
+
+        public void run(){
+            for(int i = 0; i < 100000; i++) {
+                System.out.println("Number Task is executed!");
+            }
+        }
+    }
+
