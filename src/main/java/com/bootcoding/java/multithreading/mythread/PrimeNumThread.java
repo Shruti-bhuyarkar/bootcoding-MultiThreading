fa987355a734ff300c2447d50c930123c5c62b4a
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-10-06 11:20:54 +0530

    Prime number thread

--- src/main/java/com/bootcoding/java/multithreading/mythread/PrimeNumThread.java
+++ src/main/java/com/bootcoding/java/multithreading/mythread/PrimeNumThread.java
@@ -3,7 +3,6 @@ package com.bootcoding.java.multithreading.mythread;
 public class PrimeNumThread extends  Thread{
     public void run(){
         int num = 29; // Replace with the number you want to check
-
         if (isPrime(num)) {
             System.out.println(num + " is a prime number.");
         } else {
@@ -21,15 +20,12 @@ public class PrimeNumThread extends  Thread{
         if (num % 2 == 0 || num % 3 == 0) {
             return false; // Divisible by 2 or 3, not prime
         }
-
         // Check for prime using 6k +/- 1 optimization
         for (int i = 5; i * i <= num; i += 6) {
             if (num % i == 0 || num % (i + 2) == 0) {
                 return false; // Divisible by i or i+2, not prime
             }
         }
-
         return true; // If not divisible by any number up to sqrt(num), it's prime
-
     }
 }
