2046a0bc2caed6d8907e6d8746872894073b095d
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-10-06 11:21:14 +0530

    special symbol thread

--- src/main/java/com/bootcoding/java/multithreading/mythread/SpecialSymbolTHread.java
+++ src/main/java/com/bootcoding/java/multithreading/mythread/SpecialSymbolTHread.java
@@ -1,7 +1,6 @@
 package com.bootcoding.java.multithreading.mythread;
 
 public class SpecialSymbolTHread  extends Thread{
-
         public void run(){
             for(int i = 0; i < 100000; i++) {
                 System.out.println("Special symbol Task is executed!");
