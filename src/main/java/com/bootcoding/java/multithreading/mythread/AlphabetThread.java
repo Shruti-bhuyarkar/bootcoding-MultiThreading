3ce7ecfcd38bedc983e35391e076b64afc1ea13d
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-10-06 11:20:05 +0530

    Alphabet thread

--- src/main/java/com/bootcoding/java/multithreading/mythread/AlphabetThread.java
+++ src/main/java/com/bootcoding/java/multithreading/mythread/AlphabetThread.java
@@ -1,7 +1,6 @@
 package com.bootcoding.java.multithreading.mythread;
 
 public class AlphabetThread  extends Thread {
-
     public void run() {
         for (int i = 0; i < 100000; i++) {
             System.out.println("Alphabet Task is executed!");
